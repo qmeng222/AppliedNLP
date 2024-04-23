b8513a1e4c5599497402cb073950eea648ebc58a
Qingying Meng <qmeng222@gmail.com>
2024-04-22 17:17:59 -0700

    plot GloVe word embeddings by categories

--- 010_IntroToNLP/70_word_cluster_start.py
+++ 010_IntroToNLP/70_word_cluster_start.py
@@ -9,17 +9,56 @@ import torch
 glove_dim = 100
 glove = vocab.GloVe(name='6B', dim = glove_dim)
 
-#%% Get closest words from word input
+#%% input word (str) -> index in Glove vocabulary -> embedding vector of the input word:
 def get_embedding_vector(word):
-    word_index = glove.stoi[word]
+    word_index = glove.stoi[word] # string to index
     emb = glove.vectors[word_index]
     return emb
 
 def get_closest_words_from_word(word, max_n=5):
     word_emb = get_embedding_vector(word)
-    distances = [(w, torch.dist(word_emb, get_embedding_vector(w)).cpu().item()) for w in glove.itos]
+    # a list of tuples, where each tuple contains the word from the `glove.itos` list & its corresponding distance from the input word:
+    distances = [(w, torch.dist(word_emb, get_embedding_vector(w)).cpu().item()) for w in glove.itos] # idx to str
     dist_sort_filt = sorted(distances, key=lambda x: x[1])[:max_n]
     return [item[0] for item in dist_sort_filt]
 
 get_closest_words_from_word(word='chess', max_n=10)
 
+#%%
+words = []
+categories = ["numbers", "algebra", "music", "science", "technology"]
+df_word_cloud = pd.DataFrame({
+    "category": [], # 1st col
+    "word": [] # 2nd col
+})
+
+for category in categories:
+    print(category)
+    closest_words = get_closest_words_from_word(word=category, max_n=20)
+    temp = pd.DataFrame({
+        "category": [category] * len(closest_words),
+        "word": closest_words
+    })
+
+    df_word_cloud = pd.concat([df_word_cloud, temp], ignore_index=True) # concatenate vertically
+
+# %%
+n_rows = df_word_cloud.shape[0]
+n_cols = glove_dim
+X = torch.empty((n_rows, n_cols))
+for i in range(n_rows):
+    current_word = df_word_cloud.loc[i, "word"]
+    X[i, :] = get_embedding_vector(current_word) # a vector
+    print(f"👀{i}: {current_word}")
+
+# %%
+tsne = TSNE(n_components=2)
+X_tsne = tsne.fit_transform(X.cpu().numpy())
+
+# %%
+df_word_cloud["x"] = X_tsne[:, 0]
+df_word_cloud["y"] = X_tsne[:, 1]
+
+ggplot(data=df_word_cloud.sample(25)) + aes(x = "x", y = "y", label = "word", color = "category") + geom_text() + labs(title="GloVe Word Embeddings and Categories")
+
+# %%
