81f87ce4755ab520f062c1bb5513790d896e37e2
Qingying Meng <qmeng222@gmail.com>
2024-05-07 21:19:58 -0700

    set up and execute a pipeline for text classification

--- 020_ApplyPretrainedModels/09_general_use.py
+++ 020_ApplyPretrainedModels/09_general_use.py
@@ -2,19 +2,26 @@
 from transformers import pipeline
 
 # %% only provide task
+# If no model is specified, the default model for the task + the default tokenizer of the model are selected:
 pipe = pipeline(task="text-classification")
+
 # %% run pipe
 pipe("I like it very much.")
-# %% provide model
-pipe = pipeline(task="text-classification", 
+# [{'label': 'POSITIVE', 'score': 0.9998759031295776}]
+
+# %% provide model (optional):
+pipe = pipeline(task="text-classification",
                 model="nlptown/bert-base-multilingual-uncased-sentiment")
-# %% run pipe
-# consume just a string
+
+# %%
+# consume just a string:
 pipe("I like it very much.")
+# [{'label': '5 stars', 'score': 0.5000861883163452}]
 
-# %% consume a list
-pipe(["I like it very much.", 
+# %% consume a list:
+pipe(["I like it very much.",
       "I hate it."])
-
+# [{'label': '5 stars', 'score': 0.5000861883163452},
+#  {'label': '1 star', 'score': 0.7190805077552795}]
 
 # %%
