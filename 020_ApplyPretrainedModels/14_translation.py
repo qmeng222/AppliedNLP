24e27f6f7a0a217905945c9fd415e5a5a013e807
Qingying Meng <qmeng222@gmail.com>
2024-05-08 14:07:22 -0700

    set up and execute a pipeline for text translation

--- 020_ApplyPretrainedModels/14_translation.py
+++ 020_ApplyPretrainedModels/14_translation.py
@@ -1,9 +1,13 @@
 #%% packages
 from transformers import pipeline
 
-
-
 #%% translation
 pipe = pipeline("translation_en_to_de")
 pipe("The capital of France is Paris.")
+
+# [{'translation_text': 'Die Hauptstadt Frankreichs ist Paris.'}]
+
+# Verified with Google Translate:
+# https://translate.google.com/?sl=de&tl=en&text=Die%20Hauptstadt%20Frankreichs%20ist%20Paris.&op=translate
+
 # %%
