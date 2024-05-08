2df46a2022fb79dcd3e671038dfcb62ecb8f7e91
Qingying Meng <qmeng222@gmail.com>
2024-05-07 22:26:11 -0700

    set up and execute a pipeline for question answering

--- 020_ApplyPretrainedModels/12_question_answering.py
+++ 020_ApplyPretrainedModels/12_question_answering.py
@@ -1,10 +1,13 @@
 #%% packages
 from transformers import pipeline
 
-
-
 # %% Question Answering
 pipe = pipeline("question-answering")
 pipe(context="The Big Apple is a nickname for New York City.", question="What is the Big Apple?")
+
+# {'score': 0.6228488683700562,
+#  'start': 17,
+#  'end': 45,
+#  'answer': 'a nickname for New York City'}
 
 # %%
