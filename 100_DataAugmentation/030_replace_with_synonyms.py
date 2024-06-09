2ccfd0494011c705888692f5375f5b1e18b72146
qmeng222 <qmeng222@gmail.com>
2024-06-09 14:16:24 -0700

    replace words in the text with their synonyms

--- 100_DataAugmentation/030_replace_with_synonyms.py
+++ 100_DataAugmentation/030_replace_with_synonyms.py
@@ -1,12 +1,19 @@
-#%% packages
+# %%
+# pip install nlpaug
 import nlpaug.augmenter.word as naw
+import nltk
 
 # %%
 text = "The quick brown fox jumps over the lazy dog."
-aug = naw.SynonymAug(aug_src='wordnet')
+
+#  naw library > SynonymAug class > initialize an instance | performing synonym augmentation using synonyms from WordNet:
+aug = naw.SynonymAug(aug_src="wordnet")
+
+#  replace words in the text with their synonyms based on the augmentation strategy defined in the above SynonymAug instance:
 augmented_text = aug.augment(text)
-print("Original:")
-print(text)
+print("Original:")  # The quick brown fox jumps over the lazy dog.
+print(text, "\n")
 print("Augmented Text:")
-print(augmented_text)
+print(augmented_text)  # ['The fast brownness fox jump out over the lazy dog.']
+
 # %%
