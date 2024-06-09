95e275603c756d58b72f404072ec1ad828ffc2aa
qmeng222 <qmeng222@gmail.com>
2024-06-09 14:29:22 -0700

    randomly remove words from the text (random cropping)

--- 100_DataAugmentation/040_random_crop.py
+++ 100_DataAugmentation/040_random_crop.py
@@ -1,12 +1,17 @@
-#%% packages
+# %% packages
 import nlpaug.augmenter.word as naw
 
 # %%
 text = "The quick brown fox jumps over the lazy dog."
+
+# naw library > SynonymAug class > initialize an instance for specified augmentation action (remove words from the text):
 aug = naw.RandomWordAug(action="crop")
+
+# randomly removing words from the text:
 augmented_text = aug.augment(text)
 print("Original:")
-print(text)
+print(text, "\n")  # The quick brown fox jumps over the lazy dog.
 print("Augmented Text:")
-print(augmented_text)
+print(augmented_text)  # ['The quick brown fox jumps dog.']
+
 # %%
