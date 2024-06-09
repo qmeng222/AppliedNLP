e73edda342547e2bcf5a170815b3159957c1bcc9
qmeng222 <qmeng222@gmail.com>
2024-06-09 15:03:09 -0700

    replace words in the text with similar words based on their embeddings

--- 100_DataAugmentation/060_word_embeddings.py
+++ 100_DataAugmentation/060_word_embeddings.py
@@ -1,10 +1,21 @@
-#%% packages
+# %%
 import nlpaug.augmenter.word as naw
 
 # !pip install gensim
+
+
 # %%
-augmentation = naw.WordEmbsAug(model_type='glove', model_path='glove.6B.100d.txt', action="substitute")
+# ref: https://github.com/stanfordnlp/GloVe
+# initialize an instance of the WordEmbsAug class (for replacing words in the text with similar words based on their embeddings):
+# NOTE: the `glove.6B.100d.txt` file (138MB) was downloaded from https://www.kaggle.com/datasets/sawarn69/glove6b100dtxt and saved under the same directory
+augmentation = naw.WordEmbsAug(
+    model_type="glove",  # GloVe (Global Vectors for Word Representation) embeddings model
+    model_path="glove.6B.100d.txt",  # specify the txt file containing GloVe embeddings with 100 dimensions
+    action="substitute",  # specift the augmentation action to be performed
+)
+
+
 # %%
-text_original = 'The pen is mightier than the sword.'
-augmentation.augment(text_original)
+text_original = "The pen is mightier than the sword."
+augmentation.augment(text_original)  # 'The montlanc is blander than day sword.'
 # %%
