e44a1908ff31256086cf79b24e52bf0fe222aec7
qmeng222 <qmeng222@gmail.com>
2024-06-09 14:43:51 -0700

    replace words in the text with similar words based on their context

--- 100_DataAugmentation/050_contextual_augmentation.py
+++ 100_DataAugmentation/050_contextual_augmentation.py
@@ -1,9 +1,14 @@
-#%% packages
+# %% packages
 import nlpaug.augmenter.word as naw
 
 # %%
-model_name = 'bert-base-uncased'
-augmentation = naw.ContextualWordEmbsAug(model_path=model_name, action="substitute", device='cuda')
-text_original = 'The pen is mightier than the sword.'
+# specify the pre-trained BERT model we want to use for contextual word embeddings:
+model_name = "bert-base-uncased"
+
+# naw library >  ContextualWordEmbsAug ( replace words in the text with similar words based on their context) class > init an instance:
+augmentation = naw.ContextualWordEmbsAug(model_path=model_name, action="substitute")
+
+text_original = "The pen is mightier than the sword."
 augmentation.augment(text_original)
-# %%
+# ['his pen is bigger than its sword.']
+# ['that pen is mightier against our sword.']
